2963453bd796749a7aad845159087bc2621190b4
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-07-14 11:16:00 +0100

    adding animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -77,4 +77,32 @@ $(document).ready(function () {
         }
       }
     });
+
+  //animation scroll
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeInUpBig");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeInUpBig");
+    },
+    {
+      offset: "50%",
+    }
+  );
 });
